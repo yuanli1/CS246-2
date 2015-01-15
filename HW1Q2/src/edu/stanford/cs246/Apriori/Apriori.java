b680d4400cff8d327b68cfb0089bd92fcd7e15d2
maxsonate <Max@Siamaks-iMac.local>
2015-01-15 00:28:45 -0800

    Adding finding three tuples

--- HW1Q2/src/edu/stanford/cs246/Apriori/Apriori.java
+++ HW1Q2/src/edu/stanford/cs246/Apriori/Apriori.java
@@ -11,13 +11,21 @@ import java.util.TreeSet;
 public class Apriori {
 
 	public static void main(String[] args) {
-		int supportThreshold = 2;
+		int supportThreshold = 100;
 		// Pass I:
 		// Create the ItemsCount:
 		HashMap<String, Integer> ItemsCount = new HashMap<String, Integer>();
 		ArrayList<String> PrunedItems = new ArrayList<String>();
+		int lineCounter = 0;
 		try {
+			System.out.println("Single Items:");
 			for (String line : Files.readAllLines(Paths.get(args[0]))) {
+				lineCounter++;
+				if (lineCounter % 10000 == 0) {
+					System.out.println("Processing line:" + lineCounter);
+					break;
+				}
+
 				ArrayList<String> lineItems = new ArrayList<String>();
 				// Create a list of items:
 				for (String token : line.split("\\s+")) {
@@ -35,25 +43,34 @@ public class Apriori {
 					ItemsCount.put(item, newCount);
 				}
 			}
-			for(Map.Entry<String, Integer> entry : ItemsCount.entrySet())
-			{
-				if(entry.getValue() >= supportThreshold)
-				{
+			for (Map.Entry<String, Integer> entry : ItemsCount.entrySet()) {
+				if ((entry.getValue() >= supportThreshold)
+						&& (!PrunedItems.contains(entry.getKey()))) {
 					PrunedItems.add(entry.getKey());
 				}
 			}
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		//Pass II:
+
+		// Pass II:
 		// Create the frequent Tuples of size 2:
-		HashMap<String,Integer> frequestTuplesOfSizeTwo = new HashMap<String,Integer>();
-		ArrayList<String> prunedTuplesOfSizeTwo = new ArrayList<String>();
+		System.out.println("Two Items:");
+
+		HashMap<String, Integer> frequestTuplesOfSizeTwo = new HashMap<String, Integer>(
+				10000);
+		ArrayList<String> prunedTuplesOfSizeTwo = new ArrayList<String>(10000);
 		try {
+			lineCounter = 0;
 			for (String line : Files.readAllLines(Paths.get(args[0]))) {
+				lineCounter++;
+				if (lineCounter % 10000 == 0) {
+					System.out.println("Processing line:" + lineCounter);
+					break;
+				}
+
 				ArrayList<String> lineItems = new ArrayList<String>();
 				// Create a list of items:
 				for (String token : line.split("\\s+")) {
@@ -63,9 +80,9 @@ public class Apriori {
 				}
 
 				for (int i = 0; i < lineItems.size(); i++) {
-					for (int j = i+1; j < lineItems.size(); j++) {
+					for (int j = i + 1; j < lineItems.size(); j++) {
 						if ((PrunedItems.contains(lineItems.get(i)))
-								&& (PrunedItems.contains(lineItems.get(j))))  {
+								&& (PrunedItems.contains(lineItems.get(j)))) {
 							// Create the key:
 							String sortedKey = GetSortedKey(lineItems.get(i)
 									+ "," + lineItems.get(j));
@@ -79,87 +96,107 @@ public class Apriori {
 						}
 					}
 				}
-				
-				for(Map.Entry<String, Integer> entry : frequestTuplesOfSizeTwo.entrySet())
-				{
-					if(entry.getValue() >= supportThreshold)
-					{
+
+				for (Map.Entry<String, Integer> entry : frequestTuplesOfSizeTwo
+						.entrySet()) {
+					if ((entry.getValue() >= supportThreshold)
+							&& (!prunedTuplesOfSizeTwo.contains(entry.getKey()))) {
 						prunedTuplesOfSizeTwo.add(entry.getKey());
+					}
+				}				
+			}
+			// Need to update the prune items here:
+			PrunedItems.clear();
+			for (String item : prunedTuplesOfSizeTwo) {
+				for (String token : item.split(",")) {
+					if (!PrunedItems.contains(token)) {
+						PrunedItems.add(token);
 					}
 				}
 			}
-			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		//Pass III:
-				// Create the frequent Tuples of size 3:
-				HashMap<String,Integer> frequentTuplesOfSizeThree = new HashMap<String,Integer>();
-				ArrayList<String> prunedTuplesOfSizeThree = new ArrayList<String>();
-				try {
-					for (String line : Files.readAllLines(Paths.get(args[0]))) {
-						ArrayList<String> lineItems = new ArrayList<String>();
-						// Create a list of items:
-						for (String token : line.split("\\s+")) {
-							if (!lineItems.contains(token)) {
-								lineItems.add(token);
-							}
-						}
 
-						for (int i = 0; i < lineItems.size(); i++) {
-							for (int j = i+1; j < lineItems.size(); j++) 
-								for (int k = j+1; k < lineItems.size(); k++) {
-									String pairSortedKey = GetSortedKey(lineItems.get(i) + ","
-								+ lineItems.get(j));
-										
-								if ((prunedTuplesOfSizeTwo.contains(pairSortedKey))
-										&& (PrunedItems.contains(lineItems.get(k))))  {
-									// Create the key:
-									String sortedKey = GetSortedKey(pairSortedKey
-											+ "," + lineItems.get(k));
-									int newCount = 1;
-									if (frequentTuplesOfSizeThree.containsKey(sortedKey)) {
-										newCount += frequentTuplesOfSizeThree
-												.get(sortedKey);
-									}
+		// Pass III:
+		System.out.println("Three Items:");
 
-									frequentTuplesOfSizeThree.put(sortedKey, newCount);
-								}
-							}
-						}
-						
-						for(Map.Entry<String, Integer> entry : frequentTuplesOfSizeThree.entrySet())
-						{
-							if(entry.getValue() >= supportThreshold)
-							{
-								prunedTuplesOfSizeThree.add(entry.getKey());
-							}
-						}
-					}
-					
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+		// Create the frequent Tuples of size 3:
+		HashMap<String, Integer> frequentTuplesOfSizeThree = new HashMap<String, Integer>();
+		ArrayList<String> prunedTuplesOfSizeThree = new ArrayList<String>();
+		try {
+			lineCounter = 0;
+			for (String line : Files.readAllLines(Paths.get(args[0]))) {
+				lineCounter++;
+				if (lineCounter % 10000 == 0) {
+					System.out.println("Processing line:" + lineCounter);
+					break;
 				}
+				ArrayList<String> lineItems = new ArrayList<String>();
+				// Create a list of items:
+				for (String token : line.split("\\s+")) {
+					if (!lineItems.contains(token)) {
+						lineItems.add(token);
+					}
+				}
+
+				for (int i = 0; i < lineItems.size(); i++) {
+					for (int j = i + 1; j < lineItems.size(); j++)
+						for (int k = j + 1; k < lineItems.size(); k++) {
+							String pairSortedKey = GetSortedKey(lineItems
+									.get(i) + "," + lineItems.get(j));
+							
+							//Need to skip cases like: A,B A
+							if (pairSortedKey.contains(lineItems.get(k))) {
+								continue;
+							}
+							if ((prunedTuplesOfSizeTwo.contains(pairSortedKey))
+									&& (PrunedItems.contains(lineItems.get(k)))) {
+								// Create the key:
+								String sortedKey = GetSortedKey(pairSortedKey
+										+ "," + lineItems.get(k));
+								int newCount = 1;
+								if (frequentTuplesOfSizeThree
+										.containsKey(sortedKey)) {
+									newCount += frequentTuplesOfSizeThree
+											.get(sortedKey);
+								}
+
+								frequentTuplesOfSizeThree.put(sortedKey,
+										newCount);
+							}
+						}
+				}
+
+				for (Map.Entry<String, Integer> entry : frequentTuplesOfSizeThree
+						.entrySet()) {
+					if ((entry.getValue() >= supportThreshold)
+							&& (!prunedTuplesOfSizeThree.contains(entry
+									.getKey()))) {
+						prunedTuplesOfSizeThree.add(entry.getKey());
+					}
+				}
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println(GetSortedKey("Hello,world,helllo"));
 	}
-	
-	public static String GetSortedKey(String commaSeparatedStrInput)
-	{
+
+	public static String GetSortedKey(String commaSeparatedStrInput) {
 		String outputStr = "";
-		TreeSet<String> strArray = new TreeSet<String>(); 
-		for(String item : commaSeparatedStrInput.toUpperCase().split(","))
-		{
+		TreeSet<String> strArray = new TreeSet<String>();
+		for (String item : commaSeparatedStrInput.toUpperCase().split(",")) {
 			strArray.add(item);
 		}
-		
-		for(String item : strArray)
-		{
+
+		for (String item : strArray) {
 			outputStr += item + ",";
 		}
-		
-		return outputStr.substring(0, outputStr.length()-1);
+
+		return outputStr.substring(0, outputStr.length() - 1);
 	}
 }
