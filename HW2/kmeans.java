a3deddab76d07853694e3eb0c827298a88c04709
mac.shacker <mac.shacker@gmail.com>
2015-02-02 02:12:42 -0800

    no link

--- HW2/kmeans.java
+++ HW2/kmeans.java
@@ -1,1 +1,47 @@
-/home/cloudera/workspace/kmeans/src/cs246/stanford/edu/kmeans.java
+package cs246.stanford.edu;
+
+import java.io.File;
+import java.util.Arrays;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class kmeans extends Configured implements Tool {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println(Arrays.toString(args));
+		Configuration conf = new Configuration();
+		conf.set("CentroidPath", "/home/cloudera/workspace/kmeans/c1.txt");
+		int res = ToolRunner.run(conf, new kmeans(), args);
+		System.exit(res);
+	}
+	
+	@Override
+	public int run(String[] args) throws Exception {
+
+		System.out.println(Arrays.toString(args));
+		@SuppressWarnings("deprecation")
+		Job job = new Job(getConf(), "kmeans");
+		job.setJarByClass(kmeans.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.waitForCompletion(true);
+		return 0;
+	}
+}
